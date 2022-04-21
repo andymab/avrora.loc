bb60987c11d698766e900df73ba2df9b42a3ff37
Minaev Andry <650517@355042.ru>
2022-04-22 01:03:32 +0300

    Create CatalogController and View

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CatalogController;
 use Illuminate\Support\Facades\Route;
 
 
@@ -14,7 +15,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+//Route::get('/', action: '{CatalogController@index}');
+Route::get('/', [CatalogController::class, 'index']);
